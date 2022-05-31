4b193dcd5979bff778096007ad066be90cfbcb35
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2022-06-01 01:41:01 +0700

    make footer stick to bottom

--- frontend/src/common/Footer.svelte
+++ frontend/src/common/Footer.svelte
@@ -1,16 +1,20 @@
-<div class="w-full bg-gray-800">
-  <div class="px-24 py-5">
-    <div class="flex flex-row">
-      <div class="flex grow">
-        <div class="flex flex-col">
-          <div><h1 class="font-bold text-white text-2xl">IRASSHAIMASE</h1></div>
-          <div>
-            <h1 class="font-bold text-white text-sm">いらっしゃいませ</h1>
+<div class="min-h-screen">
+  <div class="sticky top-[100vh] w-full bg-gray-800">
+    <div class="px-24 py-5">
+      <div class="flex flex-row">
+        <div class="flex grow">
+          <div class="flex flex-col">
+            <div>
+              <h1 class="font-bold text-white text-2xl">IRASSHAIMASE</h1>
+            </div>
+            <div>
+              <h1 class="font-bold text-white text-sm">いらっしゃいませ</h1>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="flex">
-        <h2 class="text-white font-bold text-lg">2022</h2>
+        <div class="flex">
+          <h2 class="text-white font-bold text-lg">2022</h2>
+        </div>
       </div>
     </div>
   </div>
