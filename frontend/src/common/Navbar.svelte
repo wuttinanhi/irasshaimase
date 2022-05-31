f46a90e8df5c9080def796b835a2c67ee952e3db
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2022-06-01 03:26:24 +0700

    add link to navbar

--- frontend/src/common/Navbar.svelte
+++ frontend/src/common/Navbar.svelte
@@ -1,21 +1,33 @@
+<script>
+</script>
+
 <div class="w-full bg-blue-500 px-16">
   <div class="py-5">
     <div class="flex flex-row">
       <div class="flex grow">
-        <div class="flex flex-col">
-          <div><h1 class="font-bold text-white text-2xl">IRASSHAIMASE</h1></div>
-          <div>
-            <h1 class="font-bold text-white text-sm">いらっしゃいませ</h1>
+        <a href="/">
+          <div class="flex flex-col">
+            <div>
+              <h1 class="font-bold text-white text-2xl">IRASSHAIMASE</h1>
+            </div>
+            <div>
+              <h1 class="font-bold text-white text-sm">いらっしゃいませ</h1>
+            </div>
           </div>
-        </div>
+        </a>
       </div>
 
-      <div class="hidden sm:flex flex-row gap-x-5">
-        <button class="p-2 font-bold text-white">CONTACT</button>
-        <button class="p-2 font-bold text-white">ABOUT</button>
-        <button class="p-2 px-5 font-bold rounded-md bg-white text-blue-600">
+      <div
+        class="hidden sm:flex flex-row gap-x-12 pt-2 content-center items-baseline"
+      >
+        <a href="/contact" class="font-bold text-white">CONTACT</a>
+        <a href="/about" class="font-bold text-white">ABOUT</a>
+        <a
+          href="/cart"
+          class="py-3 px-5 font-bold rounded-md bg-white text-blue-600"
+        >
           CART (0)
-        </button>
+        </a>
       </div>
     </div>
   </div>
