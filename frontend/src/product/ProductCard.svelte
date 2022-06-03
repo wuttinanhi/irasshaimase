525242f8fa57ae0d0dd8230d6dcf3356d140e963
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2022-06-04 06:55:42 +0700

    force image size in product card

--- frontend/src/product/ProductCard.svelte
+++ frontend/src/product/ProductCard.svelte
@@ -11,25 +11,27 @@
 
 <div class="flex flex-row basis-12/12 sm:basis-1/3 min-w-0">
   <div class="flex flex-col m-3 rounded-md border-2 border-gray-200 min-w-0">
-    <div class="flex basis-8/12 min-w-0 overflow-hidden">
+    <div class="flex flex-grow basis-8/12overflow-hidden min-w-0">
       <img
         src={productData.imageUrl}
         alt={productData.name}
-        class="object-fill w-full h-full"
+        class="flex object-fill w-[500px] h-[500px] max-h-[355px]"
         on:click={() => overlay.showOverlay()}
       />
     </div>
 
-    <div class="flex flex-col basis-4/12 px-3">
-      <div class="flex pt-3" on:click={() => overlay.showOverlay()}>
-        <p class="text-lg font-bold truncate">{productData.name}</p>
+    <div class="flex flex-shrink flex-col basis-4/12 px-3 min-w-0">
+      <div on:click={() => overlay.showOverlay()}>
+        <div class="flex pt-3">
+          <p class="text-lg font-bold truncate">{productData.name}</p>
+        </div>
+
+        <div class="flex">
+          <h3 class="flex text-lg">${productData.price}</h3>
+        </div>
       </div>
 
-      <div class="flex" on:click={() => overlay.showOverlay()}>
-        <h3 class="flex text-lg">${productData.price}</h3>
-      </div>
-
-      <div class="flex py-3">
+      <div class="flex py-3 justify-center">
         <ProductAddToCartButton {productData} />
       </div>
     </div>
