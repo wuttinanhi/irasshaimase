b846815449b9d6438697481c2c9285009c3161a0
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2022-06-13 06:02:41 +0700

    improve cart validation

--- backend/src/cart/cart.service.ts
+++ backend/src/cart/cart.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from '../product/entities/product.entity';
 import { ProductService } from '../product/product.service';
 import { Cart, CartItem } from './entities/cart.entity';
+
+export class ValidateProduct extends Product {
+  requestedQuantity: number;
+  available: boolean;
+}
 
 @Injectable()
 export class CartService {
@@ -8,28 +14,37 @@ export class CartService {
 
   private async validateCartItem(cartItem: CartItem) {
     const { productId, quantity } = cartItem;
-    const available = await this.productService.isProductAvailable(
-      productId,
-      quantity,
-    );
-    return available ? null : productId;
+    const product = await this.productService.findOne(productId);
+    const available = await this.productService.isProductAvailable(product.id, quantity);
+    const validateObject: ValidateProduct = product as any;
+    validateObject.available = available;
+    validateObject.requestedQuantity = quantity;
+    return validateObject;
   }
 
   public async validateCart(cart: Cart) {
-    const items = cart.items;
+    // remove duplicates items
+    const items = cart.items.filter((item, index) => {
+      return cart.items.findIndex((i) => i.productId === item.productId) === index;
+    });
 
-    const notAvailablePromises: Promise<number>[] = items.map((item) =>
-      this.validateCartItem(item),
-    );
+    // validate each item
+    const checkPromises = items.map((item) => this.validateCartItem(item));
+    const checkResult = await Promise.all(checkPromises);
 
-    const notAvailableResult = await Promise.all(notAvailablePromises);
+    // not available product list
+    const notAvailableList = checkResult.filter((id) => id.available === false);
 
-    // filter null
-    const notAvailable = notAvailableResult.filter((id) => id !== null);
+    // get not available count
+    const notAvailableCount = notAvailableList.length;
+
+    // not available product id list
+    const notAvailableProductIds = notAvailableList.map((v) => v.id);
 
     return {
-      notAvailableCount: notAvailable.length,
-      notAvailableProductId: notAvailable,
+      notAvailableProductIds: notAvailableProductIds,
+      notAvailableCount: notAvailableCount,
+      products: checkResult,
     };
   }
 }
