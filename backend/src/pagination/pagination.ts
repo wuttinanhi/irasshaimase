cc4a0540bd0d3676e135908322be99f0da76da17
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2022-06-22 22:22:06 +0700

    add new max limit property to pagination class

--- backend/src/pagination/pagination.ts
+++ backend/src/pagination/pagination.ts
@@ -7,19 +7,20 @@ export class Pagination<T> {
   private page: number;
   private perPage: number;
 
+  private readonly maxLimit = 50;
+
   constructor(queryBuilder: SelectQueryBuilder<T>) {
     this.queryBuilder = queryBuilder;
   }
 
   public async paginate(page: number, limit: number, raw = false) {
-    if (page < 1 || limit < 1 || limit > 50) throw new BadRequestException();
+    if (page < 1 || limit < 1 || limit > this.maxLimit) throw new BadRequestException();
 
     this.page = page;
     this.perPage = limit;
 
     const query = this.queryBuilder.skip((this.page - 1) * this.perPage).take(this.perPage);
     const items = await (raw ? query.getRawMany() : query.getMany());
-
     const totalCount = await this.queryBuilder.getCount();
 
     const response = new PaginationResponse();
