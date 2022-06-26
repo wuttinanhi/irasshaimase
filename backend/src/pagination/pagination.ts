d279b142e7a88025d68a627a34a41740e4f2c9c9
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2022-06-27 03:18:20 +0700

    fix pagination get total item error

--- backend/src/pagination/pagination.ts
+++ backend/src/pagination/pagination.ts
@@ -3,28 +3,31 @@ import { SelectQueryBuilder } from 'typeorm';
 import { PaginationResponse } from './pagination.response';
 
 export class Pagination<T> {
+  private readonly MAX_ITEM_LIMIT = 50;
+  private readonly MAX_PAGE_LIMIT = 9999;
+
   private queryBuilder: SelectQueryBuilder<T>;
   private page: number;
   private perPage: number;
-
-  private readonly maxLimit = 50;
 
   constructor(queryBuilder: SelectQueryBuilder<T>) {
     this.queryBuilder = queryBuilder;
   }
 
   public async paginate(page: number, limit: number, raw = false) {
-    if (page < 1 || limit < 1 || limit > this.maxLimit) throw new BadRequestException();
+    if (page < 1 || page > this.MAX_PAGE_LIMIT || limit < 1 || limit > this.MAX_ITEM_LIMIT) {
+      throw new BadRequestException();
+    }
 
     this.page = page;
     this.perPage = limit;
 
-    const query = this.queryBuilder.skip((this.page - 1) * this.perPage).take(this.perPage);
-    const items = await (raw ? query.getRawMany() : query.getMany());
-
     const totalCountQuery = this.queryBuilder.clone().select('COUNT(*)', 'count');
     const totalCountResult = await totalCountQuery.getRawMany();
     const totalCount = +totalCountResult[0].count;
+
+    const query = this.queryBuilder.skip((this.page - 1) * this.perPage).take(this.perPage);
+    const items = await (raw ? query.getRawMany() : query.getMany());
 
     const response = new PaginationResponse();
     response.items = items;
