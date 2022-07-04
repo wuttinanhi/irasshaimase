653d820aa8bf718c36ecb9287ae3c19af66be96b
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2022-07-05 00:37:38 +0700

    enable property whitelist on validator pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -52,6 +52,7 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
+      whitelist: true,
       transformOptions: {
         enableImplicitConversion: true,
       },
