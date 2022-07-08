255d85a09394a30a60666a4b9014a5955f9d81e6
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2022-07-08 15:24:46 +0700

    add api rate limit

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,25 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { ScheduleModule } from '@nestjs/schedule';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CartModule } from './cart/cart.module';
+import { CronjobModule } from './cronjob/cronjob.module';
+import { OrderExtendModule } from './order-extend/order-extend.module';
 import { OrderModule } from './order/order.module';
 import { PaymentModule } from './payment/payment.module';
 import { PaypalModule } from './paypal/paypal.module';
+import { PdfMakerModule } from './pdf-maker/pdf-maker.module';
 import { ProductImageModule } from './product-image/product-image.module';
 import { ProductModule } from './product/product.module';
+import { ThrottlerBehindProxyGuard } from './rate-limit/throttler-behind-proxy.guard';
+import { ReceiptModule } from './receipt/receipt.module';
 import { ShippingAddressModule } from './shipping-address/shipping-address.module';
 import { UserRoleModule } from './user-role/user-role.module';
 import { UserModule } from './user/user.module';
-import { CronjobModule } from './cronjob/cronjob.module';
-import { PdfMakerModule } from './pdf-maker/pdf-maker.module';
-import { ReceiptModule } from './receipt/receipt.module';
-import { OrderExtendModule } from './order-extend/order-extend.module';
 
 @Module({
   imports: [
@@ -44,6 +47,10 @@ import { OrderExtendModule } from './order-extend/order-extend.module';
       synchronize: true,
       autoLoadEntities: true,
     }),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 100,
+    }),
     ProductModule,
     UserModule,
     AuthModule,
@@ -61,6 +68,6 @@ import { OrderExtendModule } from './order-extend/order-extend.module';
     OrderExtendModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerBehindProxyGuard }],
 })
 export class AppModule {}
